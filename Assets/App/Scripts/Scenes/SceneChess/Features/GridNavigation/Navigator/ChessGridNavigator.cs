421cfd781dbcd61da3e4b7cd9114edbdac988df0
snapp <snapp@test.com>
2023-09-10 23:55:45 +0300

    summarizing of the pathfinding logic for each type of shapes

--- Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
+++ Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using App.Scripts.Scenes.SceneChess.Features.ChessField.GridMatrix;
 using App.Scripts.Scenes.SceneChess.Features.ChessField.Types;
+using App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator.ChessPiecesNavigation;
 using UnityEngine;
 
 namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
@@ -11,7 +12,51 @@ namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
         public List<Vector2Int> FindPath(ChessUnitType unit, Vector2Int from, Vector2Int to, ChessGrid grid)
         {
             //напиши реализацию не меняя сигнатуру функции
-            throw new NotImplementedException();
+            ChessPieceNavigation chessPieceNavigation;
+            
+            List<Vector2Int> path = new List<Vector2Int>();
+            switch (unit)
+            {
+                case ChessUnitType.Knight:
+                    chessPieceNavigation = new KnightNavigation(from, grid);
+                    path = chessPieceNavigation.GetPath(from, to);
+                    break;
+                
+                case ChessUnitType.Bishop:
+                    chessPieceNavigation = new BishopNavigation(from, grid);
+                    path = chessPieceNavigation.GetPath(from, to);
+                    break;
+                
+                case ChessUnitType.Rook:
+                    chessPieceNavigation = new RookNavigation(from, grid);
+                    path = chessPieceNavigation.GetPath(from, to);
+                    break;
+                
+                case ChessUnitType.Queen:
+                    chessPieceNavigation = new QueenNavigation(from, grid);
+                    path = chessPieceNavigation.GetPath(from, to);
+                    break;
+                
+                case ChessUnitType.Pon:
+                    chessPieceNavigation = new PawnNavigation(from, grid); // пешка это pawn (https://en.wikipedia.org/wiki/Pawn_(chess)) :)
+                    path = chessPieceNavigation.GetPath(from, to);
+                    break;
+                
+                case ChessUnitType.King:
+                    chessPieceNavigation = new KingNavigation(from, grid);
+                    path = chessPieceNavigation.GetPath(from, to);
+                    break;
+                
+                default:
+                    throw new Exception("no navigation for " + nameof(unit));
+            }
+            
+            if (path.Count == 0)
+            {
+                return null;
+            }
+            
+            return path;
         }
     }
 }
